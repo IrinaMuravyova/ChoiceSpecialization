8b570419ab726623290327beab3d91eb536e36a6
Irina Muravyeva <irinamuravyeva@MacBook-Pro-Irina.local>
2023-02-14 16:15:49 +0500

    User input of string array added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] RequestStringArray() //User input of string array
+{
+    Console.Write("Input count of string array elements: ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    String[] inputArray = new string[n];
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write($"Input element[{i}]: ");
+        inputArray[i] = Convert.ToString(Console.ReadLine());
+    }
+    return inputArray;
+}
