a9e981a86348575bb334584513d82d07355624f2
Irina Muravyeva <irinamuravyeva@MacBook-Pro-Irina.local>
2023-02-14 20:11:44 +0500

    Change array metod added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] OnlyShortStringArrayElement(string[] inputArray) // Created new array from input array elements, that lenght less than 3
+{
+    int countElementsOfInputArray = inputArray.Length;
+    int countElementsOfResult = 0;
+
+    for (int i =0; i < countElementsOfInputArray; i++)
+    {
+        if (inputArray[i].Length <= 3) {
+            countElementsOfResult++;
+        }
+    }
+    Console.WriteLine(countElementsOfResult);
+    string[] resultArray = new string[countElementsOfResult];
+    int j = 0;
+    for (int i =0; i < countElementsOfInputArray; i++)
+    {
+        if (inputArray[i].Length <= 3) {
+            resultArray[j] = inputArray[i];
+            j++;
+        }
+    }
+    return resultArray;
+}
+
+string[] inputArray = {"hello", "2", "world", ":-)"};
+string[] resultArray = 	OnlyShortStringArrayElement(inputArray);
